537ee49edbd50277fcc203574d818ae2b6189f7f
Ariel S. Boiardi <aboiardi@sissa.it>
2024-12-24 20:28:37 +0100

    Test code for Arduino Nano

--- PhotoPrintControlller_ArduinoESP32/src/main.cpp
+++ PhotoPrintControlller_ArduinoESP32/src/main.cpp
@@ -1,21 +1,82 @@
 #include <Arduino.h>
-#include <Wire.h>
 
-#define I2C_ADDRESS 0x1A
+// Constants 
+#define BAUDRATE 9600
+
+#define LED_ON    HIGH
+#define LED_OFF   LOW
+
+#define LED_0 2
+#define LED_1 3
+#define LED_2 4
+#define LED_3 5
+
+// Commands
+
+const char LED_0_ON_CMD [] = "0 on";  // LED_0_ON_CMD string.
+const char LED_1_ON_CMD [] = "1 on";  // LED_1_ON_CMD string.
+const char LED_2_ON_CMD [] = "2 on";  // LED_2_ON_CMD string.
+const char LED_3_ON_CMD [] = "3 on";  // LED_3_ON_CMD string.
+
+const char LED_0_OFF_CMD [] = "0 off"; // LED_0_OFF_CMD string.
+const char LED_1_OFF_CMD [] = "1 off"; // LED_1_OFF_CMD string.
+const char LED_2_OFF_CMD [] = "2 off"; // LED_2_OFF_CMD string.
+const char LED_3_OFF_CMD [] = "3 off"; // LED_3_OFF_CMD string.
+
+// Global valuables
+bool led_0_state = LED_OFF;
+bool led_1_state = LED_OFF;
+bool led_2_state = LED_OFF;
+bool led_3_state = LED_OFF;
+
+
+void serialEvent(){
+
+  // Read the command from the serial port
+  String command = Serial.readString();
+  command.trim();
+
+  // Check the command
+  if(command == LED_0_ON_CMD){
+    led_0_state = LED_ON;
+  } else if(command == LED_0_OFF_CMD){
+    led_0_state = LED_OFF;
+  } else if(command == LED_1_ON_CMD){
+    led_1_state = LED_ON;
+  } else if(command == LED_1_OFF_CMD){
+    led_1_state = LED_OFF;
+  } else if(command == LED_2_ON_CMD){
+    led_2_state = LED_ON;
+  } else if(command == LED_2_OFF_CMD){
+    led_2_state = LED_OFF;
+  } else if(command == LED_3_ON_CMD){
+    led_3_state = LED_ON;
+  } else if(command == LED_3_OFF_CMD){
+    led_3_state = LED_OFF;
+  }
+
+}
+
 
 void setup() {
-  Wire.begin(); // I2C0 
+  // Set up the serial communication
+  Serial.begin(BAUDRATE);
 
+  // Set up the LED pins
+  pinMode(LED_0, OUTPUT);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
 
+  // Set the initial state of the LEDs
+  digitalWrite(LED_0, led_0_state);
 }
 
 void loop() {
 
-  Wire.beginTransmission(I2C_ADDRESS); // begin transmit to device 1
-  Wire.write(0x90); // register 0x10 with bit 7 set to 1 for write mode
-  // Wire.write(0x04); // turn on blue led
-  Wire.write(0x00); // turn off blue led
-  Wire.endTransmission(); // end writing without sending stop bit
+  digitalWrite(LED_0, led_0_state);
+  digitalWrite(LED_1, led_1_state);
+  digitalWrite(LED_2, led_2_state);
+  digitalWrite(LED_3, led_3_state);
 
-  delay(100);
 }

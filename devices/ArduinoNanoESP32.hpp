f7eda79e439daacf48cf225d2da3648bf405dceb
Ariel S. Boiardi <aboiardi@sissa.it>
2025-01-15 18:38:55 +0100

    Refactor ArduinoNanoESP32 class to remove unused LED control methods and add logging for BX3SSU controller start/stop

--- devices/ArduinoNanoESP32.hpp
+++ devices/ArduinoNanoESP32.hpp
@@ -12,19 +12,13 @@ public:
 	void set_stage_speed(int speed);
 	void is_connected();
 
-	void led_on(int led_id)
-	void led_off(int led_id)
-
 private: 
 
 	asio::serial_port serial;
-	asio::streambuf serialData;
-	std::istream serialStream;
-	std::string serialMessage;
+
 };
 
-ArduinoNanoESP32::ArduinoNanoESP32(asio::io_service& io, const std::string& port_name): 
-	serial(io), 
+ArduinoNanoESP32::ArduinoNanoESP32(asio::io_service& io, const std::string& port_name): serial(io)
 {
 	// Open the serial port for Arduino Nano
 	serial.open(port_name);
@@ -33,14 +27,17 @@ ArduinoNanoESP32::ArduinoNanoESP32(asio::io_service& io, const std::string& port
 	serial.set_option(asio::serial_port::parity(asio::serial_port::parity::none));
 	serial.set_option(asio::serial_port::stop_bits(asio::serial_port::stop_bits::one));
 	serial.set_option(asio::serial_port::character_size(asio::serial_port::character_size(8)));
+
+	// Start BX3SSU from Arduino Nano
+	asio::write(serial, asio::buffer("stage -start\n", 13));
+
 };
 
 ArduinoNanoESP32::~ArduinoNanoESP32()
 {
-	serial.close();
-};
+	// Log Arduino Nano out off BX3SSU controller
+	asio::write(serial, asio::buffer("stage -stop\n", 12));
 
-void ArduinoNanoESP32::led_on(int led_id)
-{
-	asio::write(serial, asio::buffer(std::to_string(led_id)+" on"));
+	// Close the serial port
+	serial.close();
 };
